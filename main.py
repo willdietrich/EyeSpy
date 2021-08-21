95fbb1d5518c48f0d368201ba1bd21bbe8be04d2
Will Dietrich <will.a.dietrich@gmail.com>
2021-08-21 13:26:07 -0500

    Refactor API entry point and relocate discord bot.

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import discord
 from dotenv import load_dotenv
-from eyespy_client import EyeSpyClient
+from discordbot.eyespy_client import EyeSpyClient
 import os
+import uvicorn
 
-load_dotenv()
-discord_client_token = os.environ.get('DISCORD_CLIENT_TOKEN')
 
-intents = discord.Intents.default()
-intents.presences = True
-intents.members = True
+def init_discord_client():
+    discord_client_token = os.environ.get('DISCORD_CLIENT_TOKEN')
 
-client = EyeSpyClient(intents=intents)
-client.run(discord_client_token)
+    intents = discord.Intents.default()
+    intents.presences = True
+    intents.members = True
+
+    client = EyeSpyClient(intents=intents)
+    client.run(discord_client_token)
+
+def init_api():
+    uvicorn.run("api.api:app", host="127.0.0.1", port=8000, log_level="info")
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    init_discord_client()
+    init_api()
