e9f2848d7eb147e116de755da6418eb69d4365f7
Will Dietrich <will.a.dietrich@gmail.com>
2021-10-01 11:59:32 -0500

    Refactor db creation format.

--- alembic/versions/ca2d69bafecd_create_spy_table.py
+++ alembic/versions/ca2d69bafecd_create_spy_table.py
@@ -19,17 +19,29 @@ depends_on = None
 def upgrade():
     op.create_table(
         'DiscordSpys',
-        sa.Column('DiscordSpyId', sa.INTEGER, primary_key=True),
-        sa.Column('DiscordName', sa.TEXT, nullable=False)
+        sa.Column('DiscordSpyId',
+                  sa.INTEGER,
+                  primary_key=True),
+        sa.Column('DiscordName',
+                  sa.TEXT,
+                  nullable=False)
     )
 
     op.create_table(
         'DiscordSpyTargets',
-        sa.Column('DiscordSpyTargetId', sa.INTEGER, primary_key=True),
-        sa.Column('DiscordSpyId', sa.INTEGER, sa.ForeignKey('DiscordSpys.DiscordSpyId', name='fk_discordspytargets_discordspyid_discordspys'), nullable=False),
-        sa.Column('DiscordName', sa.TEXT, nullable=False)
+        sa.Column('DiscordSpyTargetId',
+                  sa.INTEGER,
+                  primary_key=True),
+        sa.Column('DiscordSpyId',
+                  sa.INTEGER,
+                  sa.ForeignKey('DiscordSpys.DiscordSpyId', name='fk_discordspytargets_discordspyid_discordspys'),
+                  nullable=False),
+        sa.Column('DiscordName',
+                  sa.TEXT,
+                  nullable=False)
     )
 
 
 def downgrade():
     op.drop_table('DiscordSpys')
+    op.drop_table('DiscordSpyTargets')
