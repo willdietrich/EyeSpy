e5af7849a058a09fe60ebcb03c7de90182df0371
Will Dietrich <will.a.dietrich@gmail.com>
2021-10-01 11:49:47 -0500

    Add migration tool and initial version of spy model.

---
+++ alembic/versions/ca2d69bafecd_create_spy_table.py
@@ -0,0 +1,35 @@
+"""create spy table
+
+Revision ID: ca2d69bafecd
+Revises: 
+Create Date: 2021-10-01 10:24:08.973713
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ca2d69bafecd'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'DiscordSpys',
+        sa.Column('DiscordSpyId', sa.INTEGER, primary_key=True),
+        sa.Column('DiscordName', sa.TEXT, nullable=False)
+    )
+
+    op.create_table(
+        'DiscordSpyTargets',
+        sa.Column('DiscordSpyTargetId', sa.INTEGER, primary_key=True),
+        sa.Column('DiscordSpyId', sa.INTEGER, sa.ForeignKey('DiscordSpys.DiscordSpyId', name='fk_discordspytargets_discordspyid_discordspys'), nullable=False),
+        sa.Column('DiscordName', sa.TEXT, nullable=False)
+    )
+
+
+def downgrade():
+    op.drop_table('DiscordSpys')
