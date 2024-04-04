11064c88a2825e15f2dfef4f61898d3074937eb0
Will Dietrich <will.a.dietrich@gmail.com>
2024-04-04 12:58:24 -0500

    Add CORS middleware.

--- eyespy/api/api.py
+++ eyespy/api/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routers import users
 
@@ -6,6 +7,23 @@ app = FastAPI()
 
 app.include_router(users.router)
 
+origins = [
+    "http://localhost",
+    "https://localhost",
+    "http://localhost:5173"
+    "https://localhost:5174"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+app.include_router(user.router)
+app.include_router(audit.router)
 
 @app.get("/", status_code=200)
 def read_root():
